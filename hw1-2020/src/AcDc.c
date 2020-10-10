850619bdf4599fe97799a1685d80758f90cca42f
kaienlin <kaienlin1999@gmail.com>
2020-10-10 14:08:00 +0800

    Delete src/cpp/ & Refactor the code

---
+++ hw1-2020/src/AcDc.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "header.h"
+
+int main( int argc, char *argv[] )
+{
+    FILE *source, *target;
+    Program program;
+    SymbolTable symtab;
+
+    if( argc == 3){
+        source = fopen(argv[1], "r");
+        target = fopen(argv[2], "w");
+        if( !source ){
+            printf("can't open the source file\n");
+            exit(2);
+        }
+        else if( !target ){
+            printf("can't open the target file\n");
+            exit(2);
+        }
+        else{
+            program = parser(source);
+            fclose(source);
+            symtab = build(program);
+            check(&program, &symtab);
+            gencode(program, target);
+        }
+    }
+    else{
+        printf("Usage: %s source_file target_file\n", argv[0]);
+    }
+
+
+    return 0;
+}
