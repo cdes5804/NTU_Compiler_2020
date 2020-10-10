b94330ad532297b6a12ffa71bd3ddfc62a47076d
TSAI MING HSUAN <joe85123@gmail.com>
2020-10-11 01:24:53 +0800

    add space for style consistency

--- hw1-2020/src/code_generation.c
+++ hw1-2020/src/code_generation.c
@@ -10,33 +10,32 @@ void fprint_op(FILE *target, ValueType op)
 {
     switch (op) {
         case MinusNode:
-            fprintf(target,"-\n");
+            fprintf(target, "-\n");
             break;
         case PlusNode:
-            fprintf(target,"+\n");
+            fprintf(target, "+\n");
             break;
         default:
-            fprintf(target,"Error in fprintf_op ValueType = %d\n",op);
+            fprintf(target, "Error in fprintf_op ValueType = %d\n", op);
             break;
     }
 }
 
 void fprint_expr( FILE *target, Expression *expr)
 {
-
     if (expr->leftOperand == NULL) {
-        switch ((expr->v).type ) {
+        switch ((expr->v).type) {
             case Identifier:
-                fprintf(target,"l%c\n",(expr->v).val.id);
+                fprintf(target, "l%c\n",(expr->v).val.id);
                 break;
             case IntConst:
-                fprintf(target,"%d\n",(expr->v).val.ivalue);
+                fprintf(target, "%d\n",(expr->v).val.ivalue);
                 break;
             case FloatConst:
-                fprintf(target,"%f\n", (expr->v).val.fvalue);
+                fprintf(target, "%f\n", (expr->v).val.fvalue);
                 break;
             default:
-                fprintf(target,"Error In fprint_left_expr. (expr->v).type=%d\n",(expr->v).type);
+                fprintf(target, "Error In fprint_left_expr. (expr->v).type=%d\n", (expr->v).type);
                 break;
         }
     } else {
@@ -60,8 +59,8 @@ void gencode(Program prog, FILE * target)
         stmt = stmts->first;
         switch (stmt.type) {
             case Print:
-                fprintf(target,"l%c\n",stmt.stmt.variable);
-                fprintf(target,"p\n");
+                fprintf(target, "l%c\n",stmt.stmt.variable);
+                fprintf(target, "p\n");
                 break;
             case Assignment:
                 fprint_expr(target, stmt.stmt.assign.expr);
@@ -72,11 +71,10 @@ void gencode(Program prog, FILE * target)
                    else if(stmt.stmt.assign.type == Float){
                    fprintf(target,"5 k\n");
                    }*/
-                fprintf(target,"s%c\n",stmt.stmt.assign.id);
-                fprintf(target,"0 k\n");
+                fprintf(target, "s%c\n", stmt.stmt.assign.id);
+                fprintf(target, "0 k\n");
                 break;
         }
         stmts=stmts->rest;
     }
-
 }
